fd721fa7c472d0f20af83679ae9cab58661b7f47
谢森炯 <375789796@qq.com>
2019-06-18 13:53:34 +0800

    Add files via upload

---
+++ news/news/items.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    create_time = scrapy.Field()
+    url = scrapy.Field()
+    wap_url = scrapy.Field()
+    summary = scrapy.Field()
+    wap_summary = scrapy.Field()
+    intro = scrapy.Field()
+    keywords = scrapy.Field()
+    content = scrapy.Field()
+    source = scrapy.Field()
+    tag = scrapy.Field()
+    pass
---
+++ news/news/spiders/sina.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import json
+import random
+import scrapy
+from scrapy import Request
+from news.items import NewsItem
+from fake_useragent import UserAgent
+
+
+class SinaSpider(scrapy.Spider):
+    name = 'sina'
+    allowed_domains = ['sina.com.cn']
+    start_urls = []
+
+    ua = UserAgent(verify_ssl=False)
+    headers = {
+        'User-Agent': ua.random,
+    }
+
+    # 全部、国内、国际、社会、体育、娱乐、军事、科技、财经、股市、美股
+    lids = ['2509', '2510', '2511', '2669', '2512', '2513', '2514', '2515', '2516', '2517', '2518']
+    index = 0
+    page = 0
+
+    url = 'https://feed.mix.sina.com.cn/api/roll/get?pageid=153&lid=%s&num=50&page=%d&r=%f' % (lids[index], page, random.random())
+    start_urls.append(url)
+
+    def start_request(self):
+        yield Request(self.start_urls, headers=self.headers)
+
+    def parse(self, response):
+        print("开始爬虫。。。")
+        print(self.url)
+
+        res = response.body_as_unicode()
+        # print("res:")
+        # print(res)
+        if res == '':
+            print("response is null(403)")
+        else:
+            sites = json.loads(res)
+            if len(sites['result']['data']):
+                for news in sites['result']['data']:
+                    request = scrapy.Request(news['url'], headers=self.headers, callback=self.parse_detail)
+                    request.meta['news'] = news
+                    yield request
+                print("结束1")
+            else:
+                print("data null,url with new lid:")
+                self.index += 1
+                if self.index > len(self.lids):
+                    print("finish")
+                    return
+                self.page = 0
+                self.url = 'https://feed.mix.sina.com.cn/api/roll/get?pageid=153&lid=%s&num=50&page=%d&r=%f' % (self.lids[self.index], self.page, random.random())
+                # print(self.url)
+                yield scrapy.Request(self.url, headers=self.headers, callback=self.parse)
+                print("结束2")
+
+        self.page += 1
+        self.url = 'https://feed.mix.sina.com.cn/api/roll/get?pageid=153&lid=%s&num=50&page=%d&r=%f' % (self.lids[self.index], self.page, random.random())
+        # print(self.url)
+        yield scrapy.Request(self.url, headers=self.headers, callback=self.parse)
+        print("结束3")
+
+        pass
+
+    def parse_detail(self, response):
+        print("爬取信息。。。")
+        item = NewsItem()
+        news = response.meta['news']
+
+        item['title'] = news['title']
+        item['create_time'] = news['ctime']
+        item['url'] = news['url']
+        item['wap_url'] = news['wapurl']
+        item['summary'] = news['summary']
+        item['wap_summary'] = news['wapsummary']
+        item['intro'] = news['intro']
+        item['keywords'] = news['keywords']
+        item['content'] = ''.join(response.xpath('//*[@id="artibody"]/p/text()').extract()).replace(u'\u3000\u3000', u'\n').replace(u'\xa0\xa0', u'\n').replace(u'\r\n', u'\n').replace(u'\n\r', u'\n').strip()
+        item['source'] = '新浪'
+        # item['tag'] = 'tag'
+
+        yield item
+        pass
